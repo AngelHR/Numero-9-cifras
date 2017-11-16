efc6b321924d58426938b69441b3e1111c04acc6
angel gabriel hernandez rodrigez <angel_n_n@live.com>
2017-11-15 20:25:07 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un numero de 9 cifras");
+            string n = Console.ReadLine();
+            string[] cad = new string[9];
+            for (int i = 0; i < cad.Length; i++)
+                cad[i] = n[i]+"";
+            foreach(string item in cad)
+            {
+                Console.Write(item+" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
